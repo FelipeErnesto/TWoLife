524cf8cda3ab3c513612fb40abc6cf9e4419900a
FelipeErnesto <felipe.ernesto.campos@usp.br>
2016-08-18 15:51:12 -0300

    Inserindo identificador de fragmentos
    Pronto, mas ainda não rodei.

--- paisagem.cpp
+++ paisagem.cpp
@@ -24,12 +24,12 @@ paisagem::paisagem(double raio, int N, double angulo_visada, double passo, doubl
 			}
 		}
 		
-	//Gera uma matriz que determina o fragmento a que cada pixel pertence
+	//Atribui valores à matriz patches, que determina o fragmento a que cada pixel pertence
 	int component = 0;
-	for (int i = 0; i < row_count; ++i) 
-		for (int j = 0; j < col_count; ++j) 
-			if (!label[i][j] && this->landscape[i][j]) find_patches(i, j, ++component);
-	this->num_patches = component;		
+	for (int k = 0; k < this->numb_cells; ++k) 
+		for (int l = 0; l < this->numb_cells; ++l) 
+			if (!this->patches[k][l] && this->landscape[k][l]) find_patches(k, l, ++component);
+	this->numb_patches = component;		
 	
 	// Calculo do raio dependendo do tipo de densidade. 0 = global, 1 = local (raio), 2 = kernel.
 	if(density_type==0)
@@ -450,18 +450,20 @@ void paisagem::atualiza_habitat(individuo * const ind) const
 	ind->set_habitat(this->landscape[hx][hy]);
 }
 
-int* paisagem::find_patches(int x, int y, int current_label)
+void paisagem::find_patches(int x, int y, int current_label)
 {
-  if (x < 0 || x == row_count) return; // out of bounds
-  if (y < 0 || y == col_count) return; // out of bounds
-  if (label[x][y] || !m[x][y]) return; // already labeled or not marked with 1 in m
+  if (x < 0 || x == this->numb_cells) return; // out of bounds
+  if (y < 0 || y == this->numb_cells) return; // out of bounds
+  if (this->patches[x][y] || !this->landscape[x][y]) return; // already labeled or not marked with 1 in m
 
   // mark the current cell
-  label[x][y] = current_label;
+  this->patches[x][y] = current_label;
 
   // recursively mark the neighbors
-  for (int direction = 0; direction < 4; ++direction)
-    dfs(x + dx[direction], y + dy[direction], current_label);
+  find_patches(x + 1, y, current_label);
+  find_patches(x, y + 1, current_label);
+  find_patches(x - 1, y, current_label);
+  find_patches(x, y - 1, current_label);
 }
 
 
