0adb684ff3788e1a1a8424de821b880c494f6f6e
FelipeErnesto <felipe.ernesto.campos@usp.br>
2016-09-18 20:12:15 -0300

    Update individuo.cpp

--- individuo.cpp
+++ individuo.cpp
@@ -57,6 +57,8 @@ individuo::individuo(double x, double y, int especie, double taxa_morte,
 		this->birth_death_eq = taxa_morte+incl_d*((taxa_basal-taxa_morte)/(incl_b+incl_d));
 	}
 
+	this->patch_label = 0;
+
 	//cout << this->raio << endl;
 }
 
