7f86a40089056020f7c267144430dcaaaedcb497
FelipeErnesto <felipe.ernesto.campos@usp.br>
2016-10-27 17:05:16 -0200

    Update paisagem.hpp

--- paisagem.hpp
+++ paisagem.hpp
@@ -116,8 +116,8 @@ public:
 
 	/** Atualiza as variáveis de todos os indivíduos (ver individuo::set_vizinhos, individuo::set_habitat e individuo::update) e escolhe uma ação para ser executada. Executa a ação e atualiza o tempo do mundo de acordo \sa \ref Introdução */
     int update();//atualizador
-	int sorteia_individuo();
-	int sorteia_acao(const individuo* chosen){return chosen->sorteia_acao();}
+    individuo* sorteia_individuo();
+    int sorteia_acao(const individuo* chosen){return chosen->sorteia_acao();}
 	void realiza_acao(int lower, int acao);//vai pegar os tempos de cada individuo e informa qual foi o escolhido e manda ele fazer
 	/** Retorna o número total de indivíduos na paisagem */
     const int conta_individuos() const{return popIndividuos.size();}
